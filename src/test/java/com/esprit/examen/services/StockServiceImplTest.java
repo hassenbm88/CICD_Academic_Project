3396f29de62efdd52633cd5ab67a2fec14bcd5aa
hassenbm88 <hassen.benmansour@esprit.tn>
2022-10-11 01:51:16 +0100

    nexus

--- src/test/java/com/esprit/examen/services/StockServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/StockServiceImplTest.java
@@ -1,4 +1,4 @@
-package com.esprit.examen.services;
+/*package com.esprit.examen.services;
 
 import static org.junit.Assert.*;
 import java.util.List;
@@ -14,21 +14,21 @@ import com.esprit.examen.entities.Stock;
 public class StockServiceImplTest {
 	@Autowired
 	IStockService stockService;
-	
+
 	@Test
 	public void testAddStock() {
 	//	List<Stock> stocks = stockService.retrieveAllStocks();
 	//	int expected=stocks.size();
 		Stock s = new Stock("stock test",10,100);
 		Stock savedStock= stockService.addStock(s);
-		
+
 	//	assertEquals(expected+1, stockService.retrieveAllStocks().size());
 		assertNotNull(savedStock.getLibelleStock());
-		//stockService.deleteStock(savedStock.getIdStock());
-		
-	} 
-	
-	/*@Test
+		stockService.deleteStock(savedStock.getIdStock());
+
+	}
+
+	@Test
 	public void testAddStockOptimized() {
 
 		Stock s = new Stock("stock test",10,100);
@@ -37,18 +37,16 @@ public class StockServiceImplTest {
 		assertSame(10, savedStock.getQte());
 		assertTrue(savedStock.getQteMin()>0);
 		stockService.deleteStock(savedStock.getIdStock());
-		
-	} 
-	*/
 
+	}
 
-	/*@Test
+	@Test
 	public void testDeleteStock() {
 		Stock s = new Stock("stock test",30,60);
 		Stock savedStock= stockService.addStock(s);
 		stockService.deleteStock(savedStock.getIdStock());
 		assertNull(stockService.retrieveStock(savedStock.getIdStock()));
 	}
-*/
+//
 }
-
+*/
