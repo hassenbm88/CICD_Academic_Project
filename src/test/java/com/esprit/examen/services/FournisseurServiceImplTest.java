574eb830bf427116e1acd9794a8f240a264fdbbb
hassenbm88 <hassen.benmansour@esprit.tn>
2022-10-18 14:23:18 +0100

    test

--- src/test/java/com/esprit/examen/services/FournisseurServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/FournisseurServiceImplTest.java
@@ -1,24 +1,14 @@
 package com.esprit.examen.services;
 
-import com.esprit.examen.entities.CategorieFournisseur;
 import com.esprit.examen.entities.DetailFournisseur;
-import com.esprit.examen.entities.Facture;
 import com.esprit.examen.entities.Fournisseur;
-import com.esprit.examen.entities.SecteurActivite;
 import com.esprit.examen.repositories.DetailFournisseurRepository;
 import com.esprit.examen.repositories.FournisseurRepository;
 import com.esprit.examen.repositories.ProduitRepository;
 import com.esprit.examen.repositories.SecteurActiviteRepository;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.List;
+
 import java.util.Optional;
-import java.util.Set;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -28,14 +18,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.Assert.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.atLeast;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.mock;
+
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -59,22 +44,6 @@ public class FournisseurServiceImplTest {
     private SecteurActiviteRepository secteurActiviteRepository;
 
 
-    @Test
-    @Disabled
-    void testAddFournisseur() {
-        DetailFournisseur detailFournisseur = new DetailFournisseur();
-        detailFournisseur.setEmail("hassen.bm@esprit.tn");
-        detailFournisseur.setFournisseur(new Fournisseur());
-        detailFournisseur.setMatricule("Matricule");
-
-        Fournisseur fournisseur = new Fournisseur();
-        fournisseur.setCode("0000");
-        fournisseur.setDetailFournisseur(detailFournisseur);
-        assertSame(fournisseur, fournisseurServiceImpl.addFournisseur(fournisseur));
-        verify(fournisseurRepository).save((Fournisseur) any());
-    }
-
-
 
     @Test
     void testRetrieveFournisseur() {
@@ -93,6 +62,24 @@ public class FournisseurServiceImplTest {
         assertSame(fournisseur, fournisseurServiceImpl.retrieveFournisseur(1L));
         verify(fournisseurRepository).findById((Long) any());
     }
+
+    @Test
+    void testAddFournisseur() {
+        DetailFournisseur detailFournisseur = new DetailFournisseur();
+        detailFournisseur.setEmail("hassen.bm@esprit.tn");
+        detailFournisseur.setFournisseur(new Fournisseur());
+
+
+        Fournisseur fournisseur = new Fournisseur();
+        fournisseur.setCode("0000");
+        fournisseur.setDetailFournisseur(detailFournisseur);
+        assertSame(fournisseur, fournisseurServiceImpl.addFournisseur(fournisseur));
+        verify(fournisseurRepository).save((Fournisseur) any());
+    }
+
+
+
+
 
 
 }
